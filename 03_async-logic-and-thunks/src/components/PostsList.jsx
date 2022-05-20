665093bc3248e6edd08779e731dfd544df65b8ed
Sushant Sharma <sushant.x01@gmail.com>
2022-05-20 21:57:53 +0530

    Chapter 3: Async logic & Thunks example on fetching posts with api

---
+++ 03_async-logic-and-thunks/src/components/PostsList.jsx
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllPosts, getPostsStatus, getPostsError, fetchPosts } from "../features/posts/postsSlice";
+import { useEffect, useId } from "react";
+import PostsExcerpt from "./PostsExcerpt";
+
+const PostsList = () => {
+    const id = useId();
+    const dispatch = useDispatch();
+
+    const posts = useSelector(selectAllPosts);
+    const postsStatus = useSelector(getPostsStatus);
+    const error = useSelector(getPostsError);
+
+    useEffect(() => {
+        if (postsStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postsStatus, dispatch])
+
+    let content;
+
+    if (postsStatus === 'loading') {
+        content = <p>"Loading..."</p>
+    } else if (postsStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+        content = orderedPosts.map(post => <PostsExcerpt key={id} post={post} />);
+    } else if (postsStatus === 'failed') {
+        content = <p>{error}</p>
+    }
+
+    return (
+        <section>
+            <h2>Posts</h2>
+            {content}
+        </section>
+    )
+}
+
+export default PostsList
